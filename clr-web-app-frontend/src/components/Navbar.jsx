8de949f695f4b8a903ac6091e7b9a809b40858dc
MrLo <147477447+MrJLou@users.noreply.github.com>
2023-11-27 14:17:23 +0800

    Update Navbar.jsx

--- clr-web-app-frontend/src/components/Navbar.jsx
+++ clr-web-app-frontend/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+//Navbar.jsx
 import { useState } from 'react';
 import './Navbar.css';
 import { SearchBar } from "./Navbar/SearchBar";
@@ -17,9 +18,7 @@ function Navbar() {
                 <img src="/src\assets/A.png" className="user" alt="user" width="65"/>
             </a>
             <a href="/">
-                <svg xmlns="http://www.w3.org/2000/svg" className="cart" viewBox="0 0 24 24" width="24" height="24">
-                <path d="M3 3h2v2m12-2h6v2h-6zM3 7h18l2 10.71a1 1 0 0 1-1 .92H4a1 1 0 0 1-1-.92L2 7zM6 21a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm12 0a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm-4-14V5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"/>
-                </svg>
+                <img src="/src\assets/Cart.png" className="cart" alt="cart" width="40"/>
             </a>
 
             <div className="search-bar-container">
