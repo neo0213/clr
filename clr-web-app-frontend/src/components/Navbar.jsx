90fb176f68c1e94e7228baa2d6167a78c86e9143
MrLo <147477447+MrJLou@users.noreply.github.com>
2023-11-27 13:39:02 +0800

    Update Navbar.jsx

--- clr-web-app-frontend/src/components/Navbar.jsx
+++ clr-web-app-frontend/src/components/Navbar.jsx
@@ -1,28 +1,34 @@
-import React from "react";
+import { useState } from 'react';
+import './Navbar.css';
+import { SearchBar } from "./Navbar/SearchBar";
+import { SearchResultsList } from "./Navbar/SearchResultsList";
 
 function Navbar() {
-    
 
-    return (
-        <>
-        <div className="container d-flex justify-content-between align-items-center">
+ const [results, setResults] = useState([]);
 
-             <a href="/" className="h1">CLR</a>
-             <div class="search-container">
-                <input className="p-3 search-bar" type="text" placeholder="Search for an item" />
-                <span class="search-icon"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
-                <path d="M21 21l-6 -6"></path>
-                </svg></span>
+ return (
+    <div className="Navbar">
+        <nav>
+            <a href="/">
+                <img src="./src\assets/Clr.png" className="title" alt="title" width="70"/>
+            </a>
+            <a href="/">
+                <img src="/src\assets/A.png" className="user" alt="user" width="65"/>
+            </a>
+            <a href="/">
+                <svg xmlns="http://www.w3.org/2000/svg" className="cart" viewBox="0 0 24 24" width="24" height="24">
+                <path d="M3 3h2v2m12-2h6v2h-6zM3 7h18l2 10.71a1 1 0 0 1-1 .92H4a1 1 0 0 1-1-.92L2 7zM6 21a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm12 0a1 1 0 1 1 0-2 1 1 0 0 1 0 2zm-4-14V5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"/>
+                </svg>
+            </a>
+
+            <div className="search-bar-container">
+                <SearchBar setResults={setResults} />
+                {results && results.length > 0 && <SearchResultsList results={results} />}
             </div>
-             <div>Profile</div>
-        </div>
-        
-        </>
-    );
-
+        </nav>
+    </div>
+ );
 }
 
-export default Navbar
-
+export default Navbar;
